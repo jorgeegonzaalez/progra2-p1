2fcc8d7e0e620f20dfb22be2154c20b973b7a7fd
Jorge González <jorgeglezfdez05@gmail.com>
2024-02-19 19:35:04 +0100

    añado nuevos archivos

---
+++ src/mates/Matematicas.java
@@ -0,0 +1,25 @@
+package mates; 
+
+import java.util.Random; 
+
+public class Matematicas {
+	public static double generarNumeroPiIterativo(long pasos) {
+		long dentroCirculo = 0;
+	
+		Random rand = new Random(); 
+
+		for (long i = 0; i < pasos; i++) { 
+			double x = rand.nextDouble() * 2 - 1; 
+			double y = rand.nextDouble() * 2 - 1; 
+
+			if (x * x + y * y <= 1) {
+				dentroCirculo++:
+			}
+		}
+
+		double proporcion = (double) dentroCirculo / pasos; 
+
+		return 4.0 * proporcion; 
+
+	}
+}
